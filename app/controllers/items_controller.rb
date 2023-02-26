153a4370328a3b3026f4bbb2563e72073585b6ca
ralphasinas <53173028+ralphasinas@users.noreply.github.com>
2023-02-26 18:24:21 +0900

    コミット②

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -30,8 +30,8 @@ class ItemsController < ApplicationController
   end
 
   def update
-    @item.update(item_params)
-    if @item.valid?
+    if @item.update(item_params)
+      @item.valid?
       redirect_to item_path(item_params)
     else
       render 'edit'
