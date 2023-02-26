8fa2fd875be784df7496b846349e1bed9e7b8817
ralphasinas <53173028+ralphasinas@users.noreply.github.com>
2023-02-26 18:35:53 +0900

    タスク⑨ 商品削除機能

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -37,6 +37,15 @@ class ItemsController < ApplicationController
     end
   end
 
+  def destroy
+    if @item.user_id == current_user.id
+      @item.destroy
+      redirect_to root_path
+    else
+      redirect_to root_path
+    end
+  end
+
   private
 
   def item_params
