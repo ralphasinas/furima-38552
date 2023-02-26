55ba6384addd0cc4c08a035809dfe94770ea4d6f
ralphasinas <53173028+ralphasinas@users.noreply.github.com>
2023-02-26 19:00:28 +0900

    コミット③

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
+  before_action :authorize_owner, only: [:edit, :destroy]
 
   def index
     @items = Item.includes(:user).order("created_at DESC")
